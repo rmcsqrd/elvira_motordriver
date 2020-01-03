06234dbe0cab064d14d16c953b455ae848e22784
rmcsqrd <rmcsqrd@gmail.com>
2020-01-03 08:37:43 -0700

    added my own package.json file

--- functions/clib/Hello_World/Hello_World.c
+++ functions/clib/Hello_World/Hello_World.c
@@ -1,15 +1,13 @@
-/*
-
 // include statements
 #include <gpiod.h>
 #include <stdint.h>
 #include "delays.h"
-
 // function prototypes
+
+/*
 void main(void);
 struct gpiod_chip *output_chip;
 struct gpiod_line *output_line;
-
 void main(){
     output_chip = gpiod_chip_open_lookup(0);
     output_line = gpiod_chip_get_line(output_chip, 200);
@@ -20,31 +18,19 @@ void main(){
         sdelay(1); 
     }
 }
-
 */
 
-
-#include <gpiod.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int main(int argc, char *argv[])
-{
-	int offset;
-
-	if (argc != 3) {
-		fprintf(stderr, "usage: %s <controler> <offset>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	if (sscanf(argv[2], "%d", &offset) != 1) {
-		fprintf(stderr, "invalid offset: %s\n", argv[2]);
-		exit(EXIT_FAILURE);
-	}
-	for (;;) {
-		gpiod_ctxless_set_value(argv[1], offset, 0, 0, argv[0], NULL, NULL);
-		gpiod_ctxless_set_value(argv[1], offset, 1, 0, argv[0], NULL, NULL);
-	}
-	return EXIT_SUCCESS;
+void main(void);
+void main(){
+    struct gpiod_chip* chip = gpiod_chip_open_by_name("gpiochip0");
+    struct gpiod_line* line = gpiod_chip_get_line(chip, 200);
+    while(1){
+        gpiod_line_request_output(line, "foo", 0);
+        gpiod_chip_close(chip);
+        sdelay(1);
+        gpiod_line_request_output(line, "foo", 1);
+        gpiod_chip_close(chip);
+        sdelay(1);
+    }
 }
-
+    
--- functions/pylib/Hello_World.py
+++ functions/pylib/Hello_World.py
@@ -1,6 +1,6 @@
 # general imports
 import Jetson.GPIO as GPIO
-
+import time
 ## import from user defined functions
 import sys
 sys.path.insert(0, '/home/rmcsqrd/programming/elvira/functions')
@@ -12,6 +12,7 @@ def GPIOInit():
     GPIO.setup(29, GPIO.OUT)
     GPIO.setup(31, GPIO.OUT)
     GPIO.setup(33, GPIO.OUT)
+    GPIO.setup(32, GPIO.OUT)
 
 def GPIOClear():
     '''
@@ -32,10 +33,34 @@ def PortLEDs():
         delays.delay_ms(100)
         GPIOClear() 
 def ToggleLEDs():
-    GPIO.output(29, GPIO.HIGH)
-    GPIO.output(29, GPIO.LOW)
+    GPIO.output(31, GPIO.HIGH)
+    GPIO.output(31, GPIO.LOW)
+
+def PWM_Test():
+    pwm = GPIO.PWM(32, 100)
+    print("\nPress Ctl C to quit \n")  # Print blank line before and after message.
+    dc=0                               # set dc variable to 0 for 0%
+    pwm.start(dc)                      # Start PWM with 0% duty cycle
+
+    try:
+        while True:                      # Loop until Ctl C is pressed to stop.
+            for dc in range(0, 101, 5):    # Loop 0 to 100 stepping dc by 5 each loop
+                pwm.ChangeDutyCycle(dc)
+                time.sleep(0.05)             # wait .05 seconds at current LED brightness
+                print(dc)
+            for dc in range(95, 0, -5):    # Loop 95 to 5 stepping dc down by 5 each loop
+                pwm.ChangeDutyCycle(dc)
+                time.sleep(0.05)             # wait .05 seconds at current LED brightness
+                print(dc)
+    except KeyboardInterrupt:
+        print("Ctl C pressed - ending program")
+
+    pwm.stop()                         # stop PWM
+    GPIO.cleanup()                     # resets GPIO ports used back to input mode
+
 if __name__ == "__main__":
     GPIOInit()
     while(1):
-        # PortLEDs() # uncomment this to cycle through green status LEDs
-        ToggleLEDs() # uncomment this to toggle LEDs rapidly
+         PortLEDs() # uncomment this to cycle through green status LEDs
+        # ToggleLEDs() # uncomment this to toggle LEDs rapidly
+        # PWM_Test() # uncomment to test PWM on pin 32
